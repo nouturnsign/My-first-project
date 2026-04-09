d7e44e33192ae303f55906738e03adeee7d9a179
Eric Wang <89937277+nouturnsign@users.noreply.github.com>
2026-04-09 16:53:49 -0700

    Add double jump and obstacle course

--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -4,8 +4,11 @@ using UnityEngine.InputSystem;
 public class PlatformerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumpForce = 3f;
+    [SerializeField] private float jumpVelocity = 3f;
+    [SerializeField] private float killPlaneY = 0f;
     private Rigidbody2D rigidBody;
+    private Vector2 initialPosition;
+    private int remainingJumps = 0;
     private bool isGrounded = false;
     public int coinsCollected = 0;
 
@@ -13,6 +16,7 @@ public class PlatformerController : MonoBehaviour
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        initialPosition = rigidBody.position;
     }
 
     // Update is called once per frame
@@ -21,20 +25,34 @@ public class PlatformerController : MonoBehaviour
 
     }
 
+    void FixedUpdate()
+    {
+        if (rigidBody.position.y < killPlaneY)
+        {
+            rigidBody.position = initialPosition;
+            remainingJumps = 0;
+            isGrounded = false;
+        }
+    }
+
     void OnMove(InputValue value)
     {
         Vector2 vector = value.Get<Vector2>();
-        rigidBody.linearVelocity = new Vector2(vector.x * speed, rigidBody.linearVelocity.y);
+        rigidBody.linearVelocityX = vector.x * speed;
     }
 
     void OnJump()
     {
-        if (isGrounded)
-            rigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        if (isGrounded || remainingJumps == 1)
+        {
+            remainingJumps--;
+            rigidBody.linearVelocityY = jumpVelocity;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        remainingJumps = 2;
         isGrounded = true;
     }
 
