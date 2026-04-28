6e8afd17472ad0c4d60ac5fdfe0655d45960cc47
Eric Wang <89937277+nouturnsign@users.noreply.github.com>
2026-04-28 11:47:51 -0700

    Lecture 4 addendum

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,11 +7,16 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private string exposedMusicParam;
     [SerializeField] private Slider audioSlider;
+    // see below
+    // [SerializeField] private AudioMixerSnapshot snapshotNoBackground;
 
     public void SetMusicVolume()
     {
         // DB is logarithmic
         float volumeInDB = Mathf.Log10(Mathf.Max(audioSlider.value, 0.0001f)) * 20f;
         audioMixer.SetFloat(exposedMusicParam, volumeInDB);
+
+        // to use a snapshot
+        // snapshotNoBackground.TransitionTo(2.0f);
     }
 }
